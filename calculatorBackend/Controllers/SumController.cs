5877099096daa7ee1ca4ea3f2bef09a54162be8d
BryanSalgadoCode <bsalgado@itstrategys.com>
2022-11-21 15:52:54 -0500

    Se comprueba y se crea la API

---
+++ calculatorBackend/Controllers/SumController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace calculatorBackend.Controllers
+{
+    [Route("api/sum")]
+    [ApiController]
+    public class SumController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("add")]
+        public dynamic AddNumbers(int number1, int number2)
+        {
+            int sum;
+            sum = number1 + number2;
+            return sum;
+        }
+        
+    }
+}
